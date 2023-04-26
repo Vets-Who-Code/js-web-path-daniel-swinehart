1cd3ffa32226ad306bd94bc7a025f6e9a980f33e
Daniel Swinehart <d4570s@gmail.com>
2023-04-26 13:21:38 +0000

    initial array and for loop

--- issue6/issue6.js
+++ issue6/issue6.js
@@ -2,7 +2,9 @@ const randArray = [24, 37, 109, 54, 78];
 let counter = 12;
 
 for (x = 0; x < randArray.length; x++) {
+    console.log("randArray[" + x +"] was- " + randArray[x]);
+    console.log("Counter Value is " + counter);
     randArray[x] += counter;
+    console.log("randArray[" + x +"] is now- " + randArray[x]);
     counter++;
-    console.log("Counter Value at interation " + x + " = " + counter);
 };
