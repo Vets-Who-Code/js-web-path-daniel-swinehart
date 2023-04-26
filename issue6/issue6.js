b0614e269ca7f9968e8579df90fdf824e05f5124
Daniel Swinehart <d4570s@gmail.com>
2023-04-26 20:22:09 +0000

    finished even odd function

--- issue6/issue6.js
+++ issue6/issue6.js
@@ -1,3 +1,15 @@
+function findEvenOdd(inputArray) {
+    let x = 0;
+    while (x < inputArray.length) {
+        if ((inputArray[x] % 2) > 0) {
+            console.log("Array value at index [" + x + "] is odd. Value = " + inputArray[x]);
+        } else {
+            console.log("Array value at index [" + x + "] is even. Value = " + inputArray[x]);
+        }
+        x++;
+    }
+};
+
 const randArray = [24, 37, 109, 54, 78];
 let counter = 12;
 
@@ -5,6 +17,8 @@ for (x = 0; x < randArray.length; x++) {
     console.log("randArray[" + x +"] was- " + randArray[x]);
     console.log("Counter Value is " + counter);
     randArray[x] += counter;
-    console.log("randArray[" + x +"] is now- " + randArray[x]);
+    console.log("randArray[" + x +"] is now- " + randArray[x] + "\n\n");
     counter++;
 };
+
+findEvenOdd(randArray);
