0e787529f6abca7568066ac8203364af8f5caebd
Daniel Swinehart <d4570s@gmail.com>
2023-04-30 18:43:36 +0100

    comments for js file

--- issue6/issue6.js
+++ issue6/issue6.js
@@ -1,3 +1,4 @@
+//Function with WHILE loop to console.log array items odd/even
 function findEvenOdd(inputArray) {
     let x = 0;
     while (x < inputArray.length) {
@@ -11,14 +12,19 @@ function findEvenOdd(inputArray) {
     console.log("\n");
 };
 
+//Assign array of 5 random numbers to variable
 const randArray = [24, 37, 109, 54, 78];
+
+//Assign a number to a variable called COUNTER
 let counter = 12;
 
+//Use a FOR loop to iterate through array and add COUNTER
 for (x = 0; x < randArray.length; x++) {
     console.log("randArray[" + x +"] was- " + randArray[x]);
-    console.log("Counter Value is " + counter);
+    console.log("Counter Value is " + counter); 
     randArray[x] += counter;
     console.log("randArray[" + x +"] is now- " + randArray[x] + "\n\n");
+    //Each iteration add 1 to COUNTER
     counter++;
 };
 
@@ -26,7 +32,7 @@ for (x = 0; x < randArray.length; x++) {
 findEvenOdd(randArray);
 
 //Call function with new array of 5 random numbers
-const randArray2 = [99, 205, 65, 534, 119];
+const randArray2 = [99, 205, 65, -534, 119];
 findEvenOdd(randArray2);
 
 //Delete the 3rd item from array
@@ -39,5 +45,7 @@ randArray2.unshift("String at Front");
 
 //Add a string to end of array
 randArray2.push("String at End");
+
+//Console.log saved array
 console.log(randArray2);
 
