b23b58a5e65a7bfd21c390403a12ec7568768592
Daniel Swinehart <d4570s@gmail.com>
2023-05-11 15:50:31 +0100

    cleaned up/rm old code

--- issue8/issue8.js
+++ issue8/issue8.js
@@ -1,66 +1,15 @@
-let firstFunction = () => {
-    console.log('waiting 1 sec');
-    return new Promise((resolve) => setTimeout(resolve, 1000))
-};
-
-firstFunction().then(() => fetch('https://swapi.dev/api/people/'))
-.then((response) => response.json())
-.then((response) => {
-    console.log(response);
-})
-.catch((error) => {
-    console.log('Error during fetch: ' + error.message);
-})
-
-
-/*
-------------------------------------not as old----------------------------------
-
-let firstFunction = () => {
-    return new Promise(
-        function(resolve, reject) {
-            resolve('first func output');
-        }
-    )
-};
-
-console.log(firstFunction());
-
-let secondFunction = (dataFromFirst) => {
-    return new Promise(
-        function(resolve, reject) {
-            resolve(dataFromFirst + 'data from second');
-        }
-    )
-};
-
-firstFunction().then((data) => {
-    return secondFunction(data);
-}).then((data) => {
-    console.log(data);
-});
-
-
-fetch('https://swapi.dev/api/people/')
-    .then((response) => response.json())
-    .then((response) => {
-        console.log(response);
-    })
-    .catch((error) => {
-        console.log('Error during fetch: ' + error.message);
-    })
-
- -------------------------------------old--------------------------------------   
-
 function getData() {
-    setTimeout(sendRequest, 1000); 
-    
+    let timeOut = new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('waiting 3 sec');
+    timeOut.then(() => fetch('https://swapi.dev/api/people/'))
+        .then((response) => response.json())
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log('Error during fetch: ' + error.message);
+        })
 };
-
-function sendRequest() {
-    fetch('https://swapi.dev/api/people/')
-        .then(response => console.log(response.json))
-}
 
 //**Select html element to add download button
 let bodyElement = document.querySelector('.index-about-me');
@@ -71,5 +20,3 @@ button1.setAttribute('class', 'btn-temp');
 button1.addEventListener('click', getData);
 button1.innerText = 'Download Data';
 bodyElement.append(button1);
-
-*/
