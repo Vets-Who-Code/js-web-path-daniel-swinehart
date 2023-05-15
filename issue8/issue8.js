92bac503532c0725e545f3995d5d6a9a0a82cf6b
Daniel Swinehart <d4570s@gmail.com>
2023-05-15 22:11:53 +0100

    working div to display data

--- issue8/issue8.js
+++ issue8/issue8.js
@@ -2,30 +2,48 @@ function getData() {
     let timeOut = new Promise((resolve) => setTimeout(resolve, 1000));
     console.log('waiting 1 sec');
     timeOut.then(() => fetch('https://swapi.dev/api/people/'))
-        .then((response) => response.json())
+        .then((response) => response.json()) //**Retrieve JSON data object
         .then((response) => {
             console.log(response.results);
-            return response.results;   
+            //**Retrieve .results from data object
+            return response.results;
         })
         .then(data => {
+            //**Create 'div' and add to section .index-about-me
+            let divData = document.createElement('div');
+            divData.setAttribute('class', 'div--data');
+            bodyElement.insertBefore(divData, bodyElement.lastChild); 
+            let divElement = document.querySelector('.div--data');
+            //**Create 'ul' for each Star Wars character
             data.forEach((character) => {
                 let dataList = document.createElement('ul');
                 dataList.setAttribute('class', 'data-display');
                 Object.keys(character).forEach((key) => {
+                    //**Loop through and display character data
                     let liElement = document.createElement('li');
                     liElement.innerHTML = key + ": " + character[key];
                     dataList.append(liElement);
-                    bodyElement.append(dataList);
+                    divElement.append(dataList);
                     console.log(character[key]);
                 })
                 
             })
         })
         .catch((error) => {
+            let divData = document.createElement('div');
+            divData.setAttribute('class', 'div--data');
+            divData.innerHTML = 'Error during fetch: ' + error.message;
+            bodyElement.insertBefore(divData, bodyElement.lastChild); 
             console.log('Error during fetch: ' + error.message);
         })
 };
 
+function hideDiv() {
+    let twitterFeed = document.querySelector('.twitter-feed');
+    twitterFeed.classList.add('div--hide');
+    document.querySelector('.btn-temp').disabled = true;
+
+};
 
 //**Select html element to add download button
 let bodyElement = document.querySelector('.index-about-me');
@@ -33,6 +51,7 @@ let bodyElement = document.querySelector('.index-about-me');
 //**Add button for downloading data
 let button1 = document.createElement('button');
 button1.setAttribute('class', 'btn-temp');
+button1.addEventListener('click', hideDiv);
 button1.addEventListener('click', getData);
 button1.innerText = 'Download Data';
 bodyElement.append(button1);
