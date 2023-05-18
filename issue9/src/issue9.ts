d03c9193db56334272b2a931bc0edaad1f0dca27
Daniel Swinehart <d4570s@gmail.com>
2023-05-18 09:58:18 +0100

    added comments to ts file

--- issue9/src/issue9.ts
+++ issue9/src/issue9.ts
@@ -1,3 +1,4 @@
+//Function to add a passed number objects to passed array
 function addToArray(array: {name: string, address: string, phone: string} [], inputNumber: number) {
     for (let x = 0; x < inputNumber; x++) {
         array.push({
@@ -7,8 +8,9 @@ function addToArray(array: {name: string, address: string, phone: string} [], in
         });
     }
     return array;
-}
+};
 
+//Function to generate string of random characters of given length
 function makeRandomString(length: number) {
     let randomString = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -16,18 +18,20 @@ function makeRandomString(length: number) {
         randomString += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     return randomString;
-}
+};
 
+//Type for an array of objects with values of strings
 const objArray: {name: string, address: string, phone: string} [] = [
     {"name": "John Doe", "address": "101 SomeStreet", "phone": "5555555555"}];
 
+//Call of function to add objects to array
 console.log(addToArray(objArray, 5));
 
-let newObjArray: {name: string, address: string, phone: string} [] = [];
+//Created empty array with array type 'object'
+const newObjArray: {name: string, address: string, phone: string} [] = [];
 
-//const myFunctionVariable = addToArray(newObjArray, 10);
-
+//Created new variable calling function
 const myFunctionVariable = addToArray;
-console.log(myFunctionVariable(newObjArray, 10));
 
-//console.log(myFunctionVariable);
+//Using variable to pass array an console.log result 
+console.log(myFunctionVariable(newObjArray, 10));
