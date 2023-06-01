d76c062d445bf86fce877668374d1ff1756b717f
Daniel Swinehart <d4570s@gmail.com>
2023-06-01 18:43:20 -0500

    slideshow css

---
+++ issue9/src/about.js
@@ -0,0 +1,13 @@
+function showSlides() {
+    let slides = document.querySelectorAll(".slideshow-slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 4000);
+};
+
+let slideIndex = 0;
+showSlides();
