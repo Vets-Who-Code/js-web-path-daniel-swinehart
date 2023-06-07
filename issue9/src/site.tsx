c8ec5fd55502ea2dd8a317b5d623361c283d36d5
Daniel Swinehart <d4570s@gmail.com>
2023-06-07 17:46:52 -0500

    have ts react files working in parcel build, finally

--- issue9/src/site.tsx
+++ issue9/src/site.tsx
@@ -1,6 +1,6 @@
 //Function for mobile menu
 function toggleMenu() {
-    let div = document.querySelector(".nav-btn-container");
+    let div: HTMLElement = document.querySelector(".nav-btn-container");
     if (div.style.display === "flex") {
         div.style.display = "none";
     } else {
@@ -10,20 +10,20 @@ function toggleMenu() {
 
 //About.html slideshow function
 function showSlides() {
-    let slides = document.querySelectorAll(".slideshow-slide");
+    let slides: NodeListOf<HTMLElement> = document.querySelectorAll(".slideshow-slide");
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 4000);
 };
 
 //Mobile site nav bar
-let menuBtn = document.querySelector(".mobile-menu");
+let menuBtn: Element = document.querySelector(".mobile-menu");
 menuBtn.addEventListener('click', toggleMenu);
 
 //About.html slideshow call
-let slideIndex = 0;
+let slideIndex: number = 0;
 showSlides();
