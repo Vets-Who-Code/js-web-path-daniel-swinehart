5904828ca9eaad55b19b97e1b0f1c46ac1f5d9d5
Daniel Swinehart <d4570s@gmail.com>
2023-06-08 15:32:55 -0500

    index.jsx removed space

--- issue10/src/index.jsx
+++ issue10/src/index.jsx
@@ -1,8 +1,6 @@
-import { createRoot } from "react-dom/client";
-import { App }  from "./App";
-import { jsx } from 'react/jsx-runtime';
-import { render } from 'react-dom';
+import {createRoot} from "react-dom/client";
+import {App}  from "./App";
 
 const container = document.getElementById("app");
 const root = createRoot(container)
-root.render(<App />);
+root.render(<App/>);
