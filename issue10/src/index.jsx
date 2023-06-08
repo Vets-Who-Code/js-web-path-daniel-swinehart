56ddd3fddf0cd1c66401b62b14c3fe2f9a1035e8
Daniel Swinehart <d4570s@gmail.com>
2023-06-08 15:23:06 -0500

    index.jsx

--- issue10/src/index.jsx
+++ issue10/src/index.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { App }  from "./App";
+import { jsx } from 'react/jsx-runtime';
 
 const container = document.getElementById("app");
 const root = createRoot(container)
