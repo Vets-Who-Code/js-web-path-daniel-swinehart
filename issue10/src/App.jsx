c94fc7abc5d430043ccac80503de7ca8d6fc9333
Daniel Swinehart <d4570s@gmail.com>
2023-06-09 11:22:10 -0500

    basic react counter component added

--- issue10/src/App.jsx
+++ issue10/src/App.jsx
@@ -1,3 +1,26 @@
+import { useState } from 'react';
+
 export function App() {
-    return <h1>Hello world!</h1>;
-  }
+  const [countValue, setCountValue] = useState(0);
+
+  function addOne() {
+    setCountValue(countValue + 1);
+  };
+
+  function subtractOne() {
+    setCountValue(countValue - 1);
+  };
+
+  function resetCounter() {
+    setCountValue(countValue * 0);
+  };
+
+  return (
+    <>
+    <h1 className="count-display">Count= {countValue}</h1>
+    <button type="button" className="test" onClick={addOne}>Increase Value</button>
+    <button type="button" className="test" onClick={subtractOne}>Subtract Value</button>
+    <button type="button" className="test" onClick={resetCounter}>Reset Value</button>
+    </>
+  );
+}
