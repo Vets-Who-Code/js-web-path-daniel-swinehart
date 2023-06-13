e3b0b9c4d912cdfa670a0b4d75803a0963767291
Daniel Swinehart <d4570s@gmail.com>
2023-06-12 20:15:00 -0500

    index.html page brought over

--- issue11/nextjs-app/pages/_app.tsx
+++ issue11/nextjs-app/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
+import '../styles/styles.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
+      <Head>
+        <title>Daniel Swinehart</title>
+        <meta name="My Site Description" content="First Next.js website"/>
+      </Head>
+      <Component {...pageProps} />
+    </>  
+  );
+};
--- issue11/nextjs-app/pages/index.tsx
+++ issue11/nextjs-app/pages/index.tsx
@@ -5,114 +5,51 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="page-content grid-container grid-container--8x4 grid-container--12x5 home-pg-bg">
+      <header className="page-header">
+        <nav className="nav-bar">
+          <div className="logo-container">
+            <a href="index.html">
+              <Image src="danielLogo.svg" alt="Site Logo" width={30} height={30} />
+            </a>
+          </div>
+          <div className="mobile-menu-container">
+            <Image src="menu-mobile.svg" alt="Menu" width={40} height={30} />
+            <div className="nav-btn-container">
+              <a href="about.html" className="nav-btn-container__link about-page">About</a>
+              <a href="projects.html" className="nav-btn-container__link project-page">Projects</a>
+              <a href="contact.html" className="nav-btn-container__link contact-page">Contact</a>
+            </div>
+          </div>
+        </nav>
+        <nav className="nav-bar-lg">
+          <div className="logo-container">
+            <a href="index.html">
+              <Image src="danielLogo.svg" alt="Site Logo" width={100} height={100} />
+            </a>
+          </div>
+          <div className="nav-btn-container">
+            <a href="about.html" className="nav-btn-container__link about-page">About</a>
+            <a href="projects.html" className="nav-btn-container__link project-page">Projects</a>
+            <a href="contact.html" className="nav-btn-container__link contact-page">Contact</a>
+          </div>
+        </nav>
+      </header>
+      {/*About Me Summary Section*/}
+      <section className="grid-item index-about-me">
+        <h1 className="main-heading">Hi there! My name is Daniel.</h1>
+        <p className="text-summary">Web development is where my passions for problem-solving and creativity intersect. I'm fascinated by the constant evolution of technology and web development, and I'm constantly challenging myself to learn new techniques and tools.</p>
+        <div className="twitter-feed">
+          <a className="twitter-timeline" data-lang="en" data-theme="light" href="https://twitter.com/ThePracticalDev?ref_src=twsrc%5Etfw">Tweets by ThePracticalDev</a> <script async src="https://platform.twitter.com/widgets.js"></script>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+      </section>
+      {/*Footer section*/}
+      <footer className="grid-item grid-footer">
+        <div className="footer-content">
+          <img className="footer-logo"src="/images/danielLogo.svg"/>
+          <p><small>Copyright &copy 2023 Daniel Swinehart</small></p>
+        </div>
+      </footer>
+    </main> 
+  );
+};
