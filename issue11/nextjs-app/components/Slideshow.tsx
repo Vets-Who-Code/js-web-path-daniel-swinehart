d6e90a19beffe487ee442f3b6dcf7c3a4ce49914
Daniel Swinehart <d4570s@gmail.com>
2023-06-14 09:07:05 -0500

    slideshow working

---
+++ issue11/nextjs-app/components/Slideshow.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+export default function Slideshow() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    //slideshow images
+    const slideImages = [
+        "/slideshow/imgOne.jpg",
+        "/slideshow/imgTwo.jpg",
+        "/slideshow/imgThree.jpg",
+        "/slideshow/imgFour.jpg",
+        "/slideshow/imgFive.jpg",
+        "/slideshow/imgSix.jpg",
+    ];
+
+    setTimeout(changeSlides, 4000);
+    
+    function changeSlides() {
+        if (currentIndex === (slideImages.length - 1)) {
+            setCurrentIndex(0);
+        } else {
+            setCurrentIndex(currentIndex + 1);
+        }
+    };
+  
+    return (
+        <div className="slideshow-container">
+            <img className="slide-img" src={slideImages[currentIndex]} alt="Slide Image" />
+        </div>
+    );
+  };
--- issue11/nextjs-app/pages/about.tsx
+++ issue11/nextjs-app/pages/about.tsx
@@ -1,26 +1,13 @@
 import { Inter } from 'next/font/google';
 import Header from '../components/Header';
 import FooterGrid from '../components/FooterGrid';
+import Slideshow from '../components/Slideshow';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function About() {
-    //About.html slideshow function
-    function showSlides() {
-    let slides = document.querySelectorAll(".slideshow-slide") as NodeListOf<HTMLElement>;
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 4000);
-    };
-
-    //About.html slideshow call
-    let slideIndex = 0;
-    showSlides();
-
+    
   return (
     <>
     <main className="page-content grid-container grid-container--8x4 grid-container--12x5 about-pg-bg">
@@ -35,32 +22,7 @@ export default function About() {
             <br />
             <br />    
             I am excited about working with new clients, and I am eager to collaborate with you to create stunning websites that represent your vision. Contact me today, and let's bring your ideas to life!</p>
-            <div className="slideshow-container">
-                <div className="slideshow-slide fade">
-                    <img className="slide-img" src="images/slideshow/imgOne.jpg" />
-                    <div className="slide-description">Fez, Morocco</div>
-                </div>
-                <div className="slideshow-slide fade">
-                    <img className="slide-img" src="images/slideshow/imgTwo.jpg" />
-                    <div className="slide-description">Meknes, Morocco</div>
-                </div>
-                <div className="slideshow-slide fade">
-                    <img className="slide-img" src="images/slideshow/imgThree.jpg" />
-                    <div className="slide-description">London, England</div>
-                </div>
-                <div className="slideshow-slide fade">
-                    <img className="slide-img" src="images/slideshow/imgFour.jpg" />
-                    <div className="slide-description">Costa Adeje, Tenerife</div>
-                </div>
-                <div className="slideshow-slide fade">
-                    <img className="slide-img" src="images/slideshow/imgFive.jpg" />
-                    <div className="slide-description">Malaga, Spain</div>
-                </div>
-                <div className="slideshow-slide fade">
-                    <img className="slide-img" src="images/slideshow/imgSix.jpeg" />
-                    <div className="slide-description">San Diego, USA</div>
-                </div>
-            </div>
+            <Slideshow />
         </section>
         <FooterGrid />
     </main>
